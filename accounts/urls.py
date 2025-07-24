6d0066fc9ad71ebcd9643e4bd7169a6d039abbc7
Mikhail <10129185+damnBombardier@users.noreply.github.com>
2025-07-24 16:42:52 +1000

    Create urls.py

---
+++ accounts/urls.py
@@ -0,0 +1,17 @@
+# accounts/urls.py
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+app_name = 'accounts'
+urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('profile/<str:username>/', views.profile, name='profile'),
+    path('profile/edit/', views.edit_profile, name='edit_profile'),
+    # path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    # path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    # path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    # path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+]
